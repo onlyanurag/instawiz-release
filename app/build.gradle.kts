f759e8aa14ca83d919fc0295bfb5f0023d049351
onlyanurag <maanshichandela2007@gmail.com>
2025-04-21 19:48:13 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.brianml31.instamoon"
+    namespace = "com.instawiz.android"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.brianml31.instamoon"
+        applicationId = "com.instawiz.android"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
